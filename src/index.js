83a8d36949c99704dd1dc08ec3571a9f10f92c26
Roberto Achar <robertoachar@gmail.com>
2017-11-19 01:15:20 -0200

    feature: add express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('It works!');
+const express = require('express');
+const winston = require('winston');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ message: 'It works!' });
+});
+
+app.listen(3000, () => {
+  winston.info('Express: 3000');
+});
